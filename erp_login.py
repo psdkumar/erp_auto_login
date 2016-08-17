ba45a50384981be655fb8f5ff44170be54994b13
Sai Dheeraj <psdkumar@users.noreply.github.com>
2016-08-17 07:02:06 +0530

    Added erp_login.py

---
+++ erp_login.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class AutoLoginERP :
+	# Initialising Web Driver
+	browser = webdriver.Firefox()
+
+	def __init__(self) :
+		# Initialise Details
+		details = open('details.txt','r')
+		allDetails = details.readlines()
+		self.__lgnid   = allDetails[0][:-1]
+		self.__passwrd = allDetails[1][:-1]
+		self.__qstn1   = allDetails[2][:-1]
+		self.__ans1    = allDetails[3][:-1]
+		self.__qstn2   = allDetails[4][:-1]
+		self.__ans2    = allDetails[5][:-1]
+		self.__qstn3   = allDetails[6][:-1]
+		self.__ans3    = allDetails[7][:-1]
+		self.__title   = allDetails[8][:-1]
+		self.__dict    = {self.__qstn1 : self.__ans1, self.__qstn2 : self.__ans2, self.__qstn3 : self.__ans3}
+		details.close()
+
+	def openERP(self) :
+		# Open ERP Webpage
+		self.browser.get('http://erp.iitkgp.ernet.in/')
+		success = "Welcome to ERP" in self.browser.title
+		if success != True : self.openBrowser()
+		return self	                          
+
+	def enterDetails(self) :
+		# Enter Login Id
+		loginId = self.browser.find_element_by_id("user_id")
+		loginId.send_keys(self.__lgnid)
+
+		# Enter Password
+		password = self.browser.find_element_by_id("password")
+		password.send_keys(self.__passwrd)
+
+		# Finding Secuirty Question 
+		self.browser.implicitly_wait(20)
+		qstn = self.browser.find_element_by_xpath("//label[@for = 'answer']").text
+
+		# Finding & Entering Answer for Secuirty Question
+		ans = self.__dict[qstn]
+		self.browser.implicitly_wait(20)
+		self.answer = self.browser.find_element_by_id("answer")
+		self.answer.send_keys(ans)
+		return self
+
+	def login(self) :	
+		# Logging In
+		self.answer.send_keys(Keys.RETURN)
+		self.browser.implicitly_wait(20)
+		success = self.__title in self.browser.title
+		if success != True :
+			self.openERP().enterDetails().login()
+		return self	
+
+	def afterLogin(self) :
+		# Go to Company's list after login
+		self.browser.find_element_by_xpath("//a[@href = 'menulist.htm?module_id=26']").click()  # CDC
+		self.browser.find_element_by_xpath("//a[@class = 'text-primary']").click()              # Student
+		self.browser.implicitly_wait(20)
+		self.browser.find_element_by_xpath("//*[contains(text(), 'Application of Placement/Internship')]").click()	# Application of Placement/Internship
+
+obj = AutoLoginERP()
+obj.openERP().enterDetails().login().afterLogin()
