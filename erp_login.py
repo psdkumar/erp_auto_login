f2bf2cc3272612c4ad4c61daa6bff5d2efd1c063
Sai Dheeraj <psdkumar@users.noreply.github.com>
2016-08-23 03:34:39 +0530

    slight change

--- erp_login.py
+++ erp_login.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Ke
 
 class AutoLoginERP :
 	# Initialising Web Driver
